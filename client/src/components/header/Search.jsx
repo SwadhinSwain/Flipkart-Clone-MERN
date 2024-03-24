b11906bc4271230edd98f20b52a9ce46d6360623
SwadhinSwain <chiku.swadhinranjan@gmail.com>
2024-03-25 00:06:19 +0530

    completed appbar

---
+++ client/src/components/header/Search.jsx
@@ -0,0 +1,31 @@
+import { InputBase, Box, styled } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const SearchContainer = styled(Box)`
+  background: #fff;
+  width: 30%;
+  border-radius: 2px;
+  margin-left: 10px;
+  display: flex;
+`;
+const InputSearchBase = styled(InputBase)`
+  padding-left: 12px;
+  width: 100%;
+  font-size: unset;
+`;
+const SearchIconWrapper = styled(Box)`
+  color: #2874f0;
+  padding: 5px;
+`;
+
+const Search = () => {
+  return (
+    <SearchContainer>
+      <InputSearchBase placeholder="Search For Products,Brands and More" />
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+    </SearchContainer>
+  );
+};
+export default Search;
