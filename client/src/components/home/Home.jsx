96ae6f5c0913ff0d901cccb0b7dd1dc113fdc17b
SwadhinSwain <chiku.swadhinranjan@gmail.com>
2024-05-22 17:38:07 +0530

    added slide pictures

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,7 +1,25 @@
+
 import Navbar from "./Navbar";
+import Banner from "./Banner";
+
+import { Box, styled } from '@mui/material';
+
+
+const Component = styled(Box)`
+    padding: 20px 10px;
+    background: #F2F2F2;
+`;
 
 const Home = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Component>
+        <Banner />
+      </Component>
+
+    </>
+  );
 };
 
 export default Home;
